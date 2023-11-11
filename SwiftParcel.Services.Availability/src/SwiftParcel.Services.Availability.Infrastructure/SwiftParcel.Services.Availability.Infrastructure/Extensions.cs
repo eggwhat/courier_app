6a0d513a6e2e6c4089a9d738a13707636e1629bb
Andrii Voznesenskyi <01174367@pw.edu.pl>
2023-11-11 02:23:50 +0100

    Updating the refferences in the Core and Application

--- SwiftParcel.Services.Availability/src/SwiftParcel.Services.Availability.Infrastructure/SwiftParcel.Services.Availability.Infrastructure/Extensions.cs
+++ SwiftParcel.Services.Availability/src/SwiftParcel.Services.Availability.Infrastructure/SwiftParcel.Services.Availability.Infrastructure/Extensions.cs
@@ -7,6 +7,102 @@ namespace SwiftParcel.Services.Availability.Infrastructure
 {
     public static class Extensions
     {
-        
+         public static IConveyBuilder AddInfrastructure(this IConveyBuilder builder)
+        {
+            builder.Services.AddSingleton<IEventMapper, EventMapper>();
+            builder.Services.AddTransient<IMessageBroker, MessageBroker>();
+            builder.Services.AddTransient<IResourcesRepository, ResourcesMongoRepository>();
+            builder.Services.AddTransient<ICustomersServiceClient, CustomersServiceClient>();
+            builder.Services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+            builder.Services.AddTransient<IAppContextFactory, AppContextFactory>();
+            builder.Services.AddTransient<IEventProcessor, EventProcessor>();
+            builder.Services.AddTransient(ctx => ctx.GetRequiredService<IAppContextFactory>().Create());
+            builder.Services.AddHostedService<MetricsJob>();
+            builder.Services.AddSingleton<CustomMetricsMiddleware>();
+            builder.Services.TryDecorate(typeof(ICommandHandler<>), typeof(OutboxCommandHandlerDecorator<>));
+            builder.Services.TryDecorate(typeof(IEventHandler<>), typeof(OutboxEventHandlerDecorator<>));
+            builder.Services.Scan(s => s.FromAssemblies(AppDomain.CurrentDomain.GetAssemblies())
+                .AddClasses(c => c.AssignableTo(typeof(IDomainEventHandler<>)))
+                .AsImplementedInterfaces()
+                .WithTransientLifetime());
+
+            return builder
+                .AddErrorHandler<ExceptionToResponseMapper>()
+                .AddQueryHandlers()
+                .AddInMemoryQueryDispatcher()
+                .AddHttpClient()
+                .AddConsul()
+                .AddFabio()
+                .AddRabbitMq(plugins: p => p.AddJaegerRabbitMqPlugin())
+                .AddMessageOutbox(o => o.AddMongo())
+                .AddExceptionToMessageMapper<ExceptionToMessageMapper>()
+                .AddMongo()
+                .AddRedis()
+                .AddMetrics()
+                .AddJaeger()
+                .AddJaegerDecorators()
+                .AddHandlersLogging()
+                .AddMongoRepository<ResourceDocument, Guid>("resources")
+                .AddWebApiSwaggerDocs()
+                .AddCertificateAuthentication()
+                .AddSecurity();
+        }
+
+        public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
+        {
+            app.UseErrorHandler()
+                .UseSwaggerDocs()
+                .UseJaeger()
+                .UseConvey()
+                .UsePublicContracts<ContractAttribute>()
+                .UseMetrics()
+                .UseMiddleware<CustomMetricsMiddleware>()
+                .UseCertificateAuthentication()
+                .UseRabbitMq()
+                .SubscribeCommand<AddResource>()
+                .SubscribeCommand<DeleteResource>()
+                .SubscribeCommand<ReleaseResourceReservation>()
+                .SubscribeCommand<ReserveResource>()
+                .SubscribeEvent<CustomerCreated>()
+                .SubscribeEvent<VehicleDeleted>();
+
+            return app;
+        }
+
+        internal static CorrelationContext GetCorrelationContext(this IHttpContextAccessor accessor)
+            => accessor.HttpContext?.Request.Headers.TryGetValue("Correlation-Context", out var json) is true
+                ? JsonConvert.DeserializeObject<CorrelationContext>(json.FirstOrDefault())
+                : null;
+
+        internal static IDictionary<string, object> GetHeadersToForward(this IMessageProperties messageProperties)
+        {
+            const string sagaHeader = "Saga";
+            if (messageProperties?.Headers is null || !messageProperties.Headers.TryGetValue(sagaHeader, out var saga))
+            {
+                return null;
+            }
+
+            return saga is null
+                ? null
+                : new Dictionary<string, object>
+                {
+                    [sagaHeader] = saga
+                };
+        }
+
+        internal static string GetSpanContext(this IMessageProperties messageProperties, string header)
+        {
+            if (messageProperties is null)
+            {
+                return string.Empty;
+            }
+
+            if (messageProperties.Headers.TryGetValue(header, out var span) && span is byte[] spanBytes)
+            {
+                return Encoding.UTF8.GetString(spanBytes);
+            }
+
+            return string.Empty;
+        }
     }
 }
