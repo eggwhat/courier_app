177b422ebbf542aa1cff31a60ca8b7abcd0f607b
Andrii Voznesenskyi <01174367@pw.edu.pl>
2023-11-13 01:15:57 +0100

    Adding the basic structure of the orders creator

--- SwiftParcel.Services.OrdersCreator/src/SwiftParcel.Services.OrdersCreator/Program.cs
+++ SwiftParcel.Services.OrdersCreator/src/SwiftParcel.Services.OrdersCreator/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// TODO: update the following to to make it use the controllers and the defauld project structure
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
