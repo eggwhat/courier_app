61b1d588f7701c1aa9290d48d497f06834124b7f
Andrii Voznesenskyi <01174367@pw.edu.pl>
2023-11-13 02:09:58 +0100

    OrdersCreator: adding the saga and the api requests handler

--- SwiftParcel.Services.OrdersCreator/src/SwiftParcel.Services.OrdersCreator/Handlers/AIOrderMakingHandler.cs
+++ SwiftParcel.Services.OrdersCreator/src/SwiftParcel.Services.OrdersCreator/Handlers/AIOrderMakingHandler.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Chronicle;
 using Convey.CQRS.Commands;
 using Convey.CQRS.Events;
 using SwiftParcel.Services.OrdersCreator.Commands;
