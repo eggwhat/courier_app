30fb9688016239774fc9b0e9de62f083c9036174
Andrii Voznesenskyi <01174367@pw.edu.pl>
2023-11-13 02:41:37 +0100

    OrdersCreator: resolveing the isseu in the solution

--- SwiftParcel.Services.OrdersCreator/scripts/start.sh
+++ SwiftParcel.Services.OrdersCreator/scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export ASPNETCORE_ENVIRONMENT=Development
-cd ../src/SwiftParcel.Services.OrdersCreator/SwiftParcel.Services.OrdersCreator
+cd ../src/SwiftParcel.Services.OrdersCreator
 dotnet run
