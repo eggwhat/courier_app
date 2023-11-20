4d824452402991e0e67fec0a1bb2b231a189d3f8
Andrii Voznesenskyi <01174367@pw.edu.pl>
2023-11-20 19:21:22 +0100

    Couriers microservice: opening the service

--- SwiftParcel.Services.Couriers/src/SwiftParcel.Services.Couriers.Api/SwiftParcel.Services.Couriers.Api/Program.cs
+++ SwiftParcel.Services.Couriers/src/SwiftParcel.Services.Couriers.Api/SwiftParcel.Services.Couriers.Api/Program.cs
@@ -1,25 +1,47 @@
-var builder = WebApplication.CreateBuilder(args);
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Convey;
+using Convey.WebApi;
+using SwiftParcel.Services.Couriers.Application;
+using SwiftParcel.Services.Couriers.Infrastructure;
+using Convey.WebApi.CQRS;
+using Convey.Types;
+using SwiftParcel.Services.Couriers.Application.Queries;
+using SwiftParcel.Services.Couriers.Application.DTO;
+using Convey.CQRS.Queries;
+using SwiftParcel.Services.Couriers.Application.Commands;
+using Convey.Logging;
+using Convey.Secrets.Vault;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace SwiftParcel.Services.Couriers.Api
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static async Task Main(string[] args)
+            => await WebHost.CreateDefaultBuilder(args)
+                .ConfigureServices(services => services
+                    .AddConvey()
+                    .AddWebApi()
+                    .AddApplication()
+                    .AddInfrastructure()
+                    .Build())
+                .Configure(app => app
+                    .UseInfrastructure()
+                    .UseDispatcherEndpoints(endpoints => endpoints
+                        .Get("", ctx => ctx.Response.WriteAsync(ctx.RequestServices.GetService<AppOptions>().Name))
+                        .Get<GetCourier, CourierDto>("couriers/{courierId}")
+                        .Get<SearchCouriers, PagedResult<CourierDto>>("couriers")
+                        .Post<AddCourier>("couriers",
+                            afterDispatch: (cmd, ctx) => ctx.Response.Created($"couriers/{cmd.CourierId}"))
+                        .Put<UpdateCourier>("couriers/{courierId}")
+                        .Delete<DeleteCourier>("couriers/{courierId}")
+                    ))
+                .UseLogging()
+                .UseVault()
+                .Build()
+                .RunAsync();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
