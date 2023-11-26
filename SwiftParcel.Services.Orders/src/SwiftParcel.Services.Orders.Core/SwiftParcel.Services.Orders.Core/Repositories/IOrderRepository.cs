e99ad904a7e4927a60eb0324b2d4acf72fa17055
eggwhat <piotr.padamczyk@gmail.com>
2023-11-27 00:38:24 +0100

    #31: add repositories for Orders.Core

---
+++ SwiftParcel.Services.Orders/src/SwiftParcel.Services.Orders.Core/SwiftParcel.Services.Orders.Core/Repositories/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace SwiftParcel.Services.Orders.Core.Repositories
+{
+    public interface IOrderRepository
+    {
+        Task<Order> GetAsync(Guid id);
+        Task<Order> GetAsync(Guid currierId, DateTime deliveryDate);
+        Task<Order> GetContainingParcelAsync(Guid parcelId);
+        Task AddAsync(Order order);
+        Task UpdateAsync(Order order);
+        Task DeleteAsync(Guid id);
+    }
+}
+
+
