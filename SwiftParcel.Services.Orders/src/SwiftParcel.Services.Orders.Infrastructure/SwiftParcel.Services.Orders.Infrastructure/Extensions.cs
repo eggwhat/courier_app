091ca75f8fb7141185c8502a4b3f1095246854da
Andrii Voznesenskyi <01174367@pw.edu.pl>
2023-11-27 02:31:42 +0100

    #31: Order.Service.Infrastructure -> updaing the basic structure, adding the refferences

--- SwiftParcel.Services.Orders/src/SwiftParcel.Services.Orders.Infrastructure/SwiftParcel.Services.Orders.Infrastructure/Extensions.cs
+++ SwiftParcel.Services.Orders/src/SwiftParcel.Services.Orders.Infrastructure/SwiftParcel.Services.Orders.Infrastructure/Extensions.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SwiftParcel.Services.Orders.Api
+namespace SwiftParcel.Services.Orders.Infrastructure
 {
-    public class Program
+    public static class Extensions
     {
         
     }
