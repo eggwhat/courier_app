832450de9ca57fa9506635ab7be070e3a806a702
eggwhat <piotr.padamczyk@gmail.com>
2023-11-27 02:14:26 +0100

    #31: add clients to services in Orders.Application

---
+++ SwiftParcel.Services.Orders/src/SwiftParcel.Services.Orders.Application/SwiftParcel.Services.Orders.Application/Services/Clients/IParcelServiceClient.cs
@@ -0,0 +1,10 @@
+﻿using SwiftParcel.Services.Orders.Application.DTO;
+
+namespace SwiftParcel.Services.Orders.Application.Services.Clients
+{
+    public interface IParcelServiceClient
+    {
+        Task<ParcelDto> GetAsync(Guid id);
+    }
+}
+
