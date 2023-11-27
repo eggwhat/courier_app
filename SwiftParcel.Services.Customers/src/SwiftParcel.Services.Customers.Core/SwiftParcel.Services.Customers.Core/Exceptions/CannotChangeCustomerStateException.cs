ce66fa088932ea03ecb3cc4776adcd7a52095416
Andrii Voznesenskyi <01174367@pw.edu.pl>
2023-11-27 03:38:36 +0100

    #37: Customers.Services -> Adding the Customer Entity

---
+++ SwiftParcel.Services.Customers/src/SwiftParcel.Services.Customers.Core/SwiftParcel.Services.Customers.Core/Exceptions/CannotChangeCustomerStateException.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SwiftParcel.Services.Customers.Core.Exceptions;
+
+namespace SwiftParcel.Services.Customers.Core
+{
+    public class CannotChangeCustomerStateException : DomainException
+    {
+        public override string Code { get; } = "cannot_change_customer_state";
+        public Guid Id { get; }
+        public State State { get; }
+
+        public CannotChangeCustomerStateException(Guid id, State state) : base(
+            $"Cannot change customer: {id} state to: {state}.")
+        {
+            Id = id;
+            State = state;
+        }
+    }
+}
