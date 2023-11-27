f7228780673bad22bac26d15a65b820eb060e123
eggwhat <piotr.padamczyk@gmail.com>
2023-11-27 16:01:24 +0100

    #31: Orders.Application -> add events

---
+++ SwiftParcel.Services.Orders/src/SwiftParcel.Services.Orders.Application/SwiftParcel.Services.Orders.Application/Events/CourierAssignedToOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Convey.CQRS.Events;
+
+namespace SwiftParcel.Services.Orders.Application.Events
+{
+    public class CourierAssignedToOrder : IEvent
+    {
+        public Guid OrderId { get; }
+        public Guid CourierId {get;}
+        public CourierAssignedToOrder(Guid orderId, Guid courierId)
+        {
+            OrderId = orderId;
+            CourierId = courierId;
+        }
+    }
+}
---
+++ SwiftParcel.Services.Orders/src/SwiftParcel.Services.Orders.Application/SwiftParcel.Services.Orders.Application/Events/OrderCreated.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Convey.CQRS.Events;
+
+namespace SwiftParcel.Services.Orders.Application.Events
+{
+    public class OrderCreated : IEvent
+    {
+        public Guid OrderId { get; }
+        public OrderCreated(Guid orderId)
+        {
+            OrderId = orderId;
+        }
+    }
+}
