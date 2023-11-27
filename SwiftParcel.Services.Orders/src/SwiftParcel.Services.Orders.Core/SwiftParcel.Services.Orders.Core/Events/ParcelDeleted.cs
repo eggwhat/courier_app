3bc0ba9845b3ca946a82b8cc701ebbfd4e4c76a3
eggwhat <piotr.padamczyk@gmail.com>
2023-11-27 01:40:59 +0100

    #31: add events in Orders.Core

---
+++ SwiftParcel.Services.Orders/src/SwiftParcel.Services.Orders.Core/SwiftParcel.Services.Orders.Core/Events/ParcelDeleted.cs
@@ -0,0 +1,18 @@
+﻿using SwiftParcel.Services.Orders.Core.Entities;
+
+namespace SwiftParcel.Services.Orders.Core.Events
+{
+    public class ParcelDeleted : IDomainEvent
+    {
+        public Order Order { get; }
+        public Parcel Parcel { get; }
+
+        public ParcelDeleted(Order order, Parcel parcel)
+        {
+            Order = order;
+            Parcel = parcel;
+        }
+    }
+}
+
+
