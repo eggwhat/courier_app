ccd60f4c57f64d27b8ab50223fb4c4bfe9bc88b9
Andrii Voznesenskyi <01174367@pw.edu.pl>
2023-11-27 03:45:32 +0100

    #37: Customers.Services -> adding the core entities, events and services

--- SwiftParcel.Services.Customers/src/SwiftParcel.Services.Customers.Core/SwiftParcel.Services.Customers.Core/Entities/Customer.cs
+++ SwiftParcel.Services.Customers/src/SwiftParcel.Services.Customers.Core/SwiftParcel.Services.Customers.Core/Entities/Customer.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SwiftParcel.Services.Customers.Core.Exceptions;
 
 namespace SwiftParcel.Services.Customers.Core.Entities
 {
