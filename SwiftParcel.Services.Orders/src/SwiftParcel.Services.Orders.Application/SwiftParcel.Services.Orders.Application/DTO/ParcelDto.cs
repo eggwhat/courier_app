b3cbc33ebf30dc2a34e89989211615309d6df107
eggwhat <piotr.padamczyk@gmail.com>
2023-11-27 02:27:28 +0100

    #31: add Dtos to Orders.Application

---
+++ SwiftParcel.Services.Orders/src/SwiftParcel.Services.Orders.Application/SwiftParcel.Services.Orders.Application/DTO/ParcelDto.cs
@@ -0,0 +1,25 @@
+﻿using SwiftParcel.Services.Orders.Core.Entities;
+
+namespace SwiftParcel.Services.Orders.Application.DTO
+{
+    public class ParcelDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Variant { get; set; }
+        public string Size { get; set; }
+
+        public ParcelDto()
+        {
+        }
+
+        public ParcelDto(Parcel parcel)
+        {
+            Id = parcel.Id;
+            Name = parcel.Name;
+            Variant = parcel.Variant;
+            Size = parcel.Size;
+        }
+    }
+}
+
