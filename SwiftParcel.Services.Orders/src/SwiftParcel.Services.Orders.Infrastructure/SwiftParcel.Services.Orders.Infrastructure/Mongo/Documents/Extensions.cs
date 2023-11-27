272130a3d0688406a830120d78afa8f018fec8b9
eggwhat <piotr.padamczyk@gmail.com>
2023-11-27 17:22:42 +0100

    #31: Orders.Infrastructure -> add mongo documents

---
+++ SwiftParcel.Services.Orders/src/SwiftParcel.Services.Orders.Infrastructure/SwiftParcel.Services.Orders.Infrastructure/Mongo/Documents/Extensions.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using SwiftParcel.Services.Orders.Application.DTO;
+using SwiftParcel.Services.Orders.Core.Entities;
+
+namespace SwiftParcel.Services.Orders.Infrastructure.Mongo.Documents
+{
+    public static class Extensions
+    {
+        public static Order AsEntity(this OrderDocument document)
+            => new Order(document.Id, document.CustomerId, document.Status, document.CreatedAt,
+                document.Parcels.Select(p => new Parcel(p.Id, p.Name, p.Variant, p.Size)),
+                document.CourierId, document.DeliveryDate, document.TotalPrice);
+
+        public static OrderDocument AsDocument(this Order entity)
+            => new OrderDocument
+            {
+                Id = entity.Id,
+                CustomerId = entity.CustomerId,
+                CourierId = entity.CourierId,
+                Status = entity.Status,
+                CreatedAt = entity.CreatedAt,
+                DeliveryDate = entity.DeliveryDate,
+                TotalPrice = entity.TotalPrice,
+                Parcels = entity.Parcels.Select(p => new OrderDocument.Parcel
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Size = p.Size,
+                    Variant = p.Variant
+                })
+            };
+
+        public static OrderDto AsDto(this OrderDocument document)
+            => new OrderDto
+            {
+                Id = document.Id,
+                CustomerId = document.CustomerId,
+                CourierId = document.CourierId,
+                Status = document.Status.ToString().ToLowerInvariant(),
+                CreatedAt = document.CreatedAt,
+                DeliveryDate = document.DeliveryDate,
+                TotalPrice = document.TotalPrice,
+                Parcels = document.Parcels.Select(p => new ParcelDto
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Size = p.Size,
+                    Variant = p.Variant
+                })
+            };
+        
+        public static Customer AsEntity(this CustomerDocument document)
+            => new Customer(document.Id);
+
+        public static CustomerDocument AsDocument(this Customer entity)
+            => new CustomerDocument
+            {
+                Id = entity.Id
+            };
+    }
+}
