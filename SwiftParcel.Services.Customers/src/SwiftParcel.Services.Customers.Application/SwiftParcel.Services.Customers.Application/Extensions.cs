a25c5f2b8602f8b2507813da021c3cea18a78a89
Andrii Voznesenskyi <01174367@pw.edu.pl>
2023-11-28 23:25:05 +0100

    #37: updating the scripts, diavling the kv in vault

--- SwiftParcel.Services.Customers/src/SwiftParcel.Services.Customers.Application/SwiftParcel.Services.Customers.Application/Extensions.cs
+++ SwiftParcel.Services.Customers/src/SwiftParcel.Services.Customers.Application/SwiftParcel.Services.Customers.Application/Extensions.cs
@@ -2,11 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Convey;
+using Convey.CQRS.Commands;
+using Convey.CQRS.Events;
 
 namespace SwiftParcel.Services.Customers.Application
 {
     public static class Extensions
     {
-        
+        public static IConveyBuilder AddApplication(this IConveyBuilder builder)
+            => builder
+                .AddCommandHandlers()
+                .AddEventHandlers()
+                .AddInMemoryCommandDispatcher()
+                .AddInMemoryEventDispatcher();
     }
 }
