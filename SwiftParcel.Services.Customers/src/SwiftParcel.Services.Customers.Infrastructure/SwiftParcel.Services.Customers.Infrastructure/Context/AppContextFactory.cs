4e15de0ed0b6afceaf2632af1acccfea506072b1
Andrii Voznesenskyi <01174367@pw.edu.pl>
2023-11-28 22:21:24 +0100

    #37: Infrastructure -> adding the Contexts

---
+++ SwiftParcel.Services.Customers/src/SwiftParcel.Services.Customers.Infrastructure/SwiftParcel.Services.Customers.Infrastructure/Context/AppContextFactory.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwiftParcel.Services.Customers.Infrastructure.Context
+{
+    internal sealed class AppContextFactory : IAppContextFactory
+    {
+        private readonly ICorrelationContextAccessor _contextAccessor;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public AppContextFactory(ICorrelationContextAccessor contextAccessor, IHttpContextAccessor httpContextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public IAppContext Create()
+        {
+            if (_contextAccessor.CorrelationContext is {})
+            {
+                var payload = JsonConvert.SerializeObject(_contextAccessor.CorrelationContext);
+
+                return string.IsNullOrWhiteSpace(payload)
+                    ? AppContext.Empty
+                    : new AppContext(JsonConvert.DeserializeObject<CorrelationContext>(payload));
+            }
+
+            var context = _httpContextAccessor.GetCorrelationContext();
+            
+            return context is null ? AppContext.Empty : new AppContext(context);
+        }
+    }
+}
