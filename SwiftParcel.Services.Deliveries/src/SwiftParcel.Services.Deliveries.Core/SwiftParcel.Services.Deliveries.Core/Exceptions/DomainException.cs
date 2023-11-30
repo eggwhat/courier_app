9a261426d1efa2249f233ffd03277fc2bcdd349f
eggwhat <piotr.padamczyk@gmail.com>
2023-11-30 01:43:25 +0100

    #45 Deliveries.Core -> add entities and exceptions

---
+++ SwiftParcel.Services.Deliveries/src/SwiftParcel.Services.Deliveries.Core/SwiftParcel.Services.Deliveries.Core/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿namespace SwiftParcel.Services.Deliveries.Core.Exceptions
+{
+    public abstract class DomainException : Exception
+    {
+        public virtual string Code { get; }
+
+        protected DomainException(string message) : base(message)
+        {
+        }
+    }
+}
