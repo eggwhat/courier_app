8daa7dc493858f530c562db048f9cd47baa5fb9e
eggwhat <piotr.padamczyk@gmail.com>
2023-11-30 16:35:11 +0100

    #45 Deliveries.Application -> add interfaces

---
+++ SwiftParcel.Services.Deliveries/src/SwiftParcel.Services.Deliveries.Application/SwiftParcel.Services.Deliveries.Application/IIdentityContext.cs
@@ -0,0 +1,11 @@
+﻿namespace SwiftParcel.Services.Deliveries.Application
+{
+    public interface IIdentityContext
+    {
+        Guid Id { get; }
+        string Role { get; }
+        bool IsAuthenticated { get; }
+        bool IsAdmin { get; }
+        IDictionary<string, string> Claims { get; }
+    }
+}
