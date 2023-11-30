09ad8a39e313287b2d3631f51d96f4136a2107d2
eggwhat <piotr.padamczyk@gmail.com>
2023-11-30 17:06:05 +0100

    #45 Deliveries.Application -> add events

---
+++ SwiftParcel.Services.Deliveries/src/SwiftParcel.Services.Deliveries.Application/SwiftParcel.Services.Deliveries.Application/Events/DeliveryCompleted.cs
@@ -0,0 +1,18 @@
+﻿using Convey.CQRS.Events;
+
+namespace SwiftParcel.Services.Deliveries.Application.Events
+{
+    public class DeliveryCompleted : IEvent
+    {
+        public Guid DeliveryId { get; }
+        public Guid OrderId { get; }
+        public DateTime DateTime { get; }
+
+        public DeliveryCompleted(Guid deliveryId, Guid orderId, DateTime dateTime)
+        {
+            DeliveryId = deliveryId;
+            OrderId = orderId;
+            DateTime = dateTime;
+        }
+    }
+}
---
+++ SwiftParcel.Services.Deliveries/src/SwiftParcel.Services.Deliveries.Application/SwiftParcel.Services.Deliveries.Application/Events/DeliveryStarted.cs
@@ -0,0 +1,16 @@
+﻿using Convey.CQRS.Events;
+
+namespace SwiftParcel.Services.Deliveries.Application.Events
+{
+    public class DeliveryStarted : IEvent
+    {
+        public Guid OrderId { get; }
+        public DateTime DateTime { get; }
+
+        public DeliveryStarted(Guid orderId, DateTime dateTime)
+        {
+            OrderId = orderId;
+            DateTime = dateTime;
+        }
+    }
+}
