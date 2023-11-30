d73d3a8298b543a929eaef8ea414cdcc91daa929
an2508374 <an2508374@gmail.com>
2023-11-30 14:27:21 +0100

    #30: Parcels.Application -> update AddParcel command and AddParcelHandler

---
+++ SwiftParcel.Services.Parcels/src/SwiftParcel.Services.Parcels.Application/SwiftParcel.Services.Parcels.Application/Exceptions/InvalidParcelPriorityException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwiftParcel.Services.Parcels.Application.Exceptions
+{
+    public class InvalidParcelPriorityException : AppException
+    {
+        public override string Code { get; } = "invalid_parcel_priority";
+        public string Priority { get; }
+
+        public InvalidParcelPriorityException(string priority) : base($"Invalid parcel priority: {priority}")
+        {
+            Priority = priority;
+        }
+    }
+}
