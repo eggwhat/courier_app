387ad2fd1a613a49027ca01638c1865429bfaade
an2508374 <an2508374@gmail.com>
2023-11-30 15:09:38 +0100

    #30: Parcels.Infrastructure -> add CustomerDocument and CustomerMongoRepository

---
+++ SwiftParcel.Services.Parcels/src/SwiftParcel.Services.Parcels.Infrastructure/SwiftParcel.Services.Parcels.Infrastructure/Mongo/Documents/CustomerDocument.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Convey.Types;
+
+namespace SwiftParcel.Services.Parcels.Infrastructure.Mongo.Documents
+{
+    public class CustomerDocument : IIdentifiable<Guid>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ SwiftParcel.Services.Parcels/src/SwiftParcel.Services.Parcels.Infrastructure/SwiftParcel.Services.Parcels.Infrastructure/Services/DateTimeProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SwiftParcel.Services.Parcels.Application.Services;
+
+namespace SwiftParcel.Services.Parcels.Infrastructure.Services
+{
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTime Now => DateTime.UtcNow;
+    }
+}
