f0608fed4fa52f677a8af09df899ac92f9d6e58e
eggwhat <piotr.padamczyk@gmail.com>
2023-12-01 01:49:01 +0100

    #45 Deliveries.Infrastructure -> add modules

---
+++ SwiftParcel.Services.Deliveries/src/SwiftParcel.Services.Deliveries.Infrastructure/SwiftParcel.Services.Deliveries.Infrastructure/Logging/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using Convey;
+using Convey.Logging.CQRS;
+using Microsoft.Extensions.DependencyInjection;
+using SwiftParcel.Services.Deliveries.Application.Commands;
+
+namespace SwiftParcel.Services.Deliveries.Infrastructure.Logging
+{
+    internal static class Extensions
+    {
+        public static IConveyBuilder AddHandlersLogging(this IConveyBuilder builder)
+        {
+            var assembly = typeof(CompleteDelivery).Assembly;
+            
+            builder.Services.AddSingleton<IMessageToLogTemplateMapper>(new MessageToLogTemplateMapper());
+            
+            return builder
+                .AddCommandHandlersLogging(assembly)
+                .AddEventHandlersLogging(assembly);
+        }
+    }
+}
---
+++ SwiftParcel.Services.Deliveries/src/SwiftParcel.Services.Deliveries.Infrastructure/SwiftParcel.Services.Deliveries.Infrastructure/Logging/MessageToLogTemplateMapper.cs
@@ -0,0 +1,43 @@
+﻿using Convey.Logging.CQRS;
+using SwiftParcel.Services.Deliveries.Application.Commands;
+
+namespace SwiftParcel.Services.Deliveries.Infrastructure.Logging
+{
+    internal sealed class MessageToLogTemplateMapper : IMessageToLogTemplateMapper
+    {
+        private static IReadOnlyDictionary<Type, HandlerLogTemplate> MessageTemplates 
+            => new Dictionary<Type, HandlerLogTemplate>
+            {
+                {
+                    typeof(AddDeliveryRegistration), new HandlerLogTemplate
+                    {
+                        After = "Added a registration for the delivery with id: {DeliveryId}."
+                    }
+                },
+                {
+                    typeof(CompleteDelivery),  new HandlerLogTemplate
+                    {
+                        After = "Completed the delivery with id: {DeliveryId}."
+                    }
+                },
+                {
+                    typeof(FailDelivery), new HandlerLogTemplate
+                    {
+                        After = "Failed the delivery with id: {DeliveryId}, reason: {Reason}"
+                    }
+                },
+                {
+                    typeof(StartDelivery), new HandlerLogTemplate
+                    {
+                        After = "Started the delivery with id: {DeliveryId}."
+                    }
+                },
+            };
+        
+        public HandlerLogTemplate Map<TMessage>(TMessage message) where TMessage : class
+        {
+            var key = message.GetType();
+            return MessageTemplates.TryGetValue(key, out var template) ? template : null;
+        }
+    }
+}
