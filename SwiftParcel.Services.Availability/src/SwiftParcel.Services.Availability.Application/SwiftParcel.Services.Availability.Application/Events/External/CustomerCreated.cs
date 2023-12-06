21e7c2948e4fd2c91de4ed4f93aac4c0be35baf1
eggwhat <piotr.padamczyk@gmail.com>
2023-12-06 01:04:24 +0100

    #50 add routing attribute

--- SwiftParcel.Services.Availability/src/SwiftParcel.Services.Availability.Application/SwiftParcel.Services.Availability.Application/Events/External/CustomerCreated.cs
+++ SwiftParcel.Services.Availability/src/SwiftParcel.Services.Availability.Application/SwiftParcel.Services.Availability.Application/Events/External/CustomerCreated.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Convey.CQRS.Events;
+using Convey.MessageBrokers;
 
 namespace SwiftParcel.Services.Availability.Application.Events.External
 {
+    [Message("customers")]
     public class CustomerCreated : IEvent
     {
         public Guid CustomerId { get; }
--- SwiftParcel.Services.OrdersCreator/src/SwiftParcel.Services.OrdersCreator/Events/External/ResourceReserved.cs
+++ SwiftParcel.Services.OrdersCreator/src/SwiftParcel.Services.OrdersCreator/Events/External/ResourceReserved.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Convey.CQRS.Events;
+using Convey.MessageBrokers;
 
 namespace SwiftParcel.Services.OrdersCreator.Events.External
 {
+    [Message("availability")]
     public class ResourceReserved : IEvent
     {
         public Guid ResourceId { get; }
