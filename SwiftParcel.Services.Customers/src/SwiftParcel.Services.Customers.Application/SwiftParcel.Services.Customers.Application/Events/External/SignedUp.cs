3b5015ae0cff370a16a68b5ab7318fbb7c80eb96
Piotr Padamczyk <piotr.padamczyk@gmail.com>
2023-12-07 00:18:21 +0100

    #50 add routing attribute (#51)

--- SwiftParcel.Services.Customers/src/SwiftParcel.Services.Customers.Application/SwiftParcel.Services.Customers.Application/Events/External/SignedUp.cs
+++ SwiftParcel.Services.Customers/src/SwiftParcel.Services.Customers.Application/SwiftParcel.Services.Customers.Application/Events/External/SignedUp.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Convey.CQRS.Events;
+using Convey.MessageBrokers;
 
 namespace SwiftParcel.Services.Customers.Application.Events.External
 {
+    [Message("identity")]
     public class SignedUp : IEvent
     {
         public Guid UserId { get; }
--- SwiftParcel.Services.Orders/src/SwiftParcel.Services.Orders.Application/SwiftParcel.Services.Orders.Application/Events/External/ParcelDeleted.cs
+++ SwiftParcel.Services.Orders/src/SwiftParcel.Services.Orders.Application/SwiftParcel.Services.Orders.Application/Events/External/ParcelDeleted.cs
@@ -1,7 +1,9 @@
 ﻿using Convey.CQRS.Events;
+using Convey.MessageBrokers;
 
 namespace SwiftParcel.Services.Orders.Application.Events.External
 {
+    [Message("deliveries")]
     public class ParcelDeleted : IEvent
     {
         public Guid ParcelId { get; }
