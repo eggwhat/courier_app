502b8237bdcd35a9a76aab0061e261f4cf645b99
eggwhat <piotr.padamczyk@gmail.com>
2023-12-09 18:33:03 +0100

    #49: fix cancellation handler

--- SwiftParcel.Services.Orders/src/SwiftParcel.Services.Orders.Application/SwiftParcel.Services.Orders.Application/Commands/SendCancellationEmail.cs
+++ SwiftParcel.Services.Orders/src/SwiftParcel.Services.Orders.Application/SwiftParcel.Services.Orders.Application/Commands/SendCancellationEmail.cs
@@ -7,10 +7,11 @@ namespace SwiftParcel.Services.Orders.Application.Commands
         public string Subject { get; }
         public string Body { get; }
         public Guid CustomerId { get; }
+        public Guid OrderId { get; }
         public SendCancellationEmail(Guid orderId, Guid customerId)
         {
             Subject = "Order cancelled";
-            Body = $"Your order '{orderId}' has been cancelled.";
+            OrderId = orderId;
             CustomerId = customerId;
         }
     }
