89c09d45524f9853a15f788a895c7f66d7400bdc
Andrii Voznesenskyi <01174367@pw.edu.pl>
2023-12-15 01:40:26 +0100

    #61: PricingService update according to the point of Piotrek, update the basic price algorithm

---
+++ SwiftParcel.Services.Pricing/src/SwiftParcel.Services.Pricing.Api/Core/Entities/Parcel.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwiftParcel.Services.Pricing.Api.Core.Entities
+{
+    public class Parcel
+    {
+        public double Length { get; private set; }
+        public double Width { get; private set; }
+        public double Height { get; private set; }
+        public double Weight { get; private set; }
+        public bool HighPriority { get; private set; }
+        public bool DeliverAtWeekend { get; private set; }
+
+        public Parcel(double length, double width, double height, double weight, bool highPriority, bool deliverAtWeekend)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+            Weight = weight;
+            HighPriority = highPriority;
+            DeliverAtWeekend = deliverAtWeekend;
+        }
+    }
+}
