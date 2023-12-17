1488bcc5d44c6ec1892785bbeb032dc75c2e9d71
eggwhat <piotr.padamczyk@gmail.com>
2023-12-18 00:50:04 +0100

    #71: fix connection with pricing service

--- SwiftParcel.Services.Parcels/src/SwiftParcel.Services.Parcels.Infrastructure/SwiftParcel.Services.Parcels.Infrastructure/Services/Clients/PricingServiceClient.cs
+++ SwiftParcel.Services.Parcels/src/SwiftParcel.Services.Parcels.Infrastructure/SwiftParcel.Services.Parcels.Infrastructure/Services/Clients/PricingServiceClient.cs
@@ -19,6 +19,6 @@ namespace SwiftParcel.Services.Parcels.Infrastructure.Services.Clients
 
         public Task<ParcelDeliveryPricingDto> GetParcelDeliveryPriceAsync(Guid customerId, decimal orderPrice,
          double length, double width, double height, double weight, bool highPriority, bool deliverAtWeekend)
-            => _httpClient.GetAsync<ParcelDeliveryPricingDto>($"{_url}/pricing?customerId={customerId}&orderPrice={orderPrice}&length={length}&width={width}&height={height}&weight={weight}&highPriority={highPriority}&deliverAtWeekend={deliverAtWeekend}");
+            => _httpClient.GetAsync<ParcelDeliveryPricingDto>($"{_url}/pricing?customerId={customerId}&orderPrice={orderPrice}&length={length}&width={width}&height={height}&weight={weight}&highPriority={highPriority.ToString().ToLowerInvariant()}&deliverAtWeekend={deliverAtWeekend.ToString().ToLowerInvariant()}");
     }
 }
