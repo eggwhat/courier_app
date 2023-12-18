66df537d3b30141e9506e224a693df94c1e3394f
eggwhat <piotr.padamczyk@gmail.com>
2023-12-18 21:18:16 +0100

    #70: add endpoint for order status

--- SwiftParcel.Services.Orders/src/SwiftParcel.Services.Orders.Api/SwiftParcel.Services.Orders.Api/Program.cs
+++ SwiftParcel.Services.Orders/src/SwiftParcel.Services.Orders.Api/SwiftParcel.Services.Orders.Api/Program.cs
@@ -35,7 +35,7 @@ namespace SwiftParcel.Services.Orders.Api
                         .Get<GetOrder, OrderDto>("orders/{orderId}")
                         .Get<GetOrders, IEnumerable<OrderDto>>("orders")
                         .Get<GetOrdersOfficeWorker, IEnumerable<OrderDto>>("orders/office-worker")
-                        //.Get<GetOrderStatus, OrderStatusDto>("orders/{orderId}/status}")
+                        .Get<GetOrderStatus, OrderStatusDto>("orders/{orderId}/status}")
                         .Delete<DeleteOrder>("orders/{orderId}")
                         .Post<CreateOrder>("orders",
                             afterDispatch: (cmd, ctx) => ctx.Response.Created($"orders/{cmd.OrderId}"))
