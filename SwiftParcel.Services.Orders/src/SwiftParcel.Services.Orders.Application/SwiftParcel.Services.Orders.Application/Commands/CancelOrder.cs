ba9b84bf852822f61fb8664c617b3b611b6d2b87
eggwhat <piotr.padamczyk@gmail.com>
2024-01-02 13:09:50 +0100

    #101: add endpoint for cancelling order by customer

---
+++ SwiftParcel.Services.Orders/src/SwiftParcel.Services.Orders.Application/SwiftParcel.Services.Orders.Application/Commands/CancelOrder.cs
@@ -0,0 +1,14 @@
+﻿using Convey.CQRS.Commands; 
+
+namespace SwiftParcel.Services.Orders.Application.Commands
+{
+    public class CancelOrder : ICommand
+    {
+        public Guid OrderId { get; }
+
+        public CancelOrder(Guid orderId)
+        {
+            OrderId = orderId;
+        }
+    }
+}
