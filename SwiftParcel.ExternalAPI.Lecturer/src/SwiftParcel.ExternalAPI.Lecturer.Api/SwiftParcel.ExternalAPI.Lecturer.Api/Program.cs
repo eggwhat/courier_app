e0b4d7afbdc8b0407d1c6eba99a69422aba86e70
eggwhat <piotr.padamczyk@gmail.com>
2024-01-03 23:32:02 +0100

    #99: add contexts and decorators

---
+++ SwiftParcel.ExternalAPI.Lecturer/src/SwiftParcel.ExternalAPI.Lecturer.Api/SwiftParcel.ExternalAPI.Lecturer.Api/Program.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Convey;
+using Convey.Secrets.Vault;
+using Convey.Logging;
+using Convey.Types;
+using Convey.WebApi;
+using Convey.WebApi.CQRS;
+
+namespace SwiftParcel.ExternalAPI.Lecturer.Api
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+            => await WebHost.CreateDefaultBuilder(args)
+                .ConfigureServices(services => services
+                    .AddConvey()
+                    .AddWebApi()
+                    .AddApplication()
+                    .AddInfrastructure()
+                    .Build())
+                .Configure(app => app
+                    .UseSwagger()
+                    .UseSwaggerUI()
+                    .UseInfrastructure()
+                    .UseDispatcherEndpoints(endpoints => endpoints
+                        .Get("", ctx => ctx.Response.WriteAsync(ctx.RequestServices.GetService<AppOptions>().Name))
+                    ))
+                .UseLogging()
+                .UseVault()
+                .Build()
+                .RunAsync();
+    }
+}
