a4dd20e47a6da7556622f0a34252e0f6822bc51a
eggwhat <piotr.padamczyk@gmail.com>
2024-01-04 02:44:36 +0100

    #99: add client for retrieving access token

---
+++ SwiftParcel.ExternalAPI.Lecturer/src/SwiftParcel.ExternalAPI.Lecturer.Application/SwiftParcel.ExternalAPI.Lecturer.Application/Services/Clients/IIdentityManagerServiceClient.cs
@@ -0,0 +1,10 @@
+﻿using SwiftParcel.ExternalAPI.Lecturer.Application.DTO;
+using Convey.HTTP;
+
+namespace SwiftParcel.ExternalAPI.Lecturer.Application.Services.Clients
+{
+    public interface IIdentityManagerServiceClient
+    {
+        Task<HttpResult<AccessTokenDto>> PostAsync();
+    }   
+}
