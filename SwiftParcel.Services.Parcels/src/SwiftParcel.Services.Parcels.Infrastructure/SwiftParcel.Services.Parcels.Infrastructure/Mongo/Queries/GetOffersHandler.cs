d36395fc4b2baa9ba3a70a502ac20f45902323fb
eggwhat <piotr.padamczyk@gmail.com>
2024-01-06 02:58:12 +0100

    #99: fix name

--- SwiftParcel.Services.Parcels/src/SwiftParcel.Services.Parcels.Infrastructure/SwiftParcel.Services.Parcels.Infrastructure/Mongo/Queries/GetOffersHandler.cs
+++ SwiftParcel.Services.Parcels/src/SwiftParcel.Services.Parcels.Infrastructure/SwiftParcel.Services.Parcels.Infrastructure/Mongo/Queries/GetOffersHandler.cs
@@ -10,19 +10,19 @@ using SwiftParcel.Services.Parcels.Application.Services.Clients;
 
 namespace SwiftParcel.Services.Parcels.Infrastructure.Mongo.Queries
 {
-    internal sealed class GetParcelExpirationStatusHandler : IQueryHandler<GetParcelExpirationStatus, IEnumerable<ExpirationStatusDto>>
+    internal sealed class GetOffersHandler : IQueryHandler<GetOffers, IEnumerable<ExpirationStatusDto>>
     {
         private readonly IMongoRepository<ParcelDocument, Guid> _repository;
         private readonly ILecturerApiServiceClient _lecturerApiServiceClient;
         private readonly string _companyName = "SwiftParcel";
-        public GetParcelExpirationStatusHandler(IMongoRepository<ParcelDocument, Guid> repository,
+        public GetOffersHandler(IMongoRepository<ParcelDocument, Guid> repository,
             ILecturerApiServiceClient lecturerApiServiceClient)
         {
             _repository = repository;
             _lecturerApiServiceClient = lecturerApiServiceClient;
         }
 
-        public async Task<IEnumerable<ExpirationStatusDto>> HandleAsync(GetParcelExpirationStatus query, CancellationToken cancellationToken)
+        public async Task<IEnumerable<ExpirationStatusDto>> HandleAsync(GetOffers query, CancellationToken cancellationToken)
         {
             var document = await _repository.GetAsync(v => v.Id == query.ParcelId);
             var offer = new ExpirationStatusDto
@@ -30,7 +30,7 @@ namespace SwiftParcel.Services.Parcels.Infrastructure.Mongo.Queries
                 ParcelId = document.Id,
                 TotalPrice = document.CalculatedPrice,
                 ExpiringAt = document.ValidTo,
-                PriceBreakDown = null,
+                PriceBreakDown = null,    // pricing service doesn't provide that yet
                 CompanyName = _companyName
             };
             var offerLecturerApi = await _lecturerApiServiceClient.GetOfferAsync(query.ParcelId);
