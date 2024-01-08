4165256c2eec60ce1a42bab2e311c771a76979cf
eggwhat <piotr.padamczyk@gmail.com>
2024-01-09 00:15:07 +0100

    #99: add exception mapper

---
+++ SwiftParcel.ExternalAPI.Lecturer/src/SwiftParcel.ExternalAPI.Lecturer.Application/SwiftParcel.ExternalAPI.Lecturer.Application/Events/Rejected/ConfirmOrderRejected.cs
@@ -0,0 +1,18 @@
+﻿using Convey.CQRS.Events;
+
+namespace SwiftParcel.ExternalAPI.Lecturer.Application.Events.Rejected
+{
+    public class ConfirmOrderRejected : IRejectedEvent
+    {
+        public Guid OrderId { get; }
+        public string Reason { get; }
+        public string Code { get; }
+
+        public ConfirmOrderRejected (Guid orderId, string reason, string code)
+        {
+            OrderId = orderId;
+            Reason = reason;
+            Code = code;
+        }
+    }
+}
---
+++ SwiftParcel.ExternalAPI.Lecturer/src/SwiftParcel.ExternalAPI.Lecturer.Application/SwiftParcel.ExternalAPI.Lecturer.Application/Events/Rejected/CreateOrderRejected.cs
@@ -0,0 +1,22 @@
+﻿using Convey.CQRS.Events;
+
+namespace SwiftParcel.ExternalAPI.Lecturer.Application.Events.Rejected
+{
+    public class CreateOrderRejected : IRejectedEvent
+    {
+        public Guid OrderId { get; }
+        public Guid CustomerId { get; }
+        public Guid ParcelId { get; }
+        public string Reason { get; }
+        public string Code { get; }
+
+        public CreateOrderRejected(Guid orderId, Guid customerId, Guid parcelId, string reason, string code)
+        {
+            OrderId = orderId;
+            CustomerId = customerId;
+            ParcelId = parcelId;
+            Reason = reason;
+            Code = code;
+        }
+    }
+}
