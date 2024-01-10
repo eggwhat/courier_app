b5898d1ad5258888afa08fe1cf9a25934540ebad
eggwhat <piotr.padamczyk@gmail.com>
2024-01-11 00:53:31 +0100

    #99: add exception handling

--- SwiftParcel.Services.Orders/src/SwiftParcel.Services.Orders.Application/SwiftParcel.Services.Orders.Application/Commands/Handlers/ConfirmOrderMiniCurrierHandler.cs
+++ SwiftParcel.Services.Orders/src/SwiftParcel.Services.Orders.Application/SwiftParcel.Services.Orders.Application/Commands/Handlers/ConfirmOrderMiniCurrierHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Convey.CQRS.Commands;
 using SwiftParcel.Services.Orders.Application.Services.Clients;
+using SwiftParcel.Services.Orders.Application.Exceptions;
 
 namespace SwiftParcel.Services.Orders.Application.Commands.Handlers
 {
@@ -13,7 +14,15 @@ namespace SwiftParcel.Services.Orders.Application.Commands.Handlers
 
         public async Task HandleAsync(ConfirmOrderMiniCurrier command, CancellationToken cancellationToken)
         {
-            await _lecturerApiServiceClient.PostConfirmOrder(command.OrderId.ToString());
+            var response = await _lecturerApiServiceClient.PostConfirmOrder(command.OrderId.ToString());
+            if (response == null)
+            {
+                throw new LecturerApiServiceConnectionException();
+            }
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new LecturerApiServiceException(response.ReasonPhrase);
+            }
         }
     }
 }
