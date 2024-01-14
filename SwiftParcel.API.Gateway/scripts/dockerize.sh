70b7de9031dae937f1f24a627afd5a257046c0e7
Andrii Voznesenskyi <01174367@pw.edu.pl>
2024-01-14 15:31:05 +0100

    #110: transfering deployment to droplet

--- SwiftParcel.API.Gateway/scripts/dockerize.sh
+++ SwiftParcel.API.Gateway/scripts/dockerize.sh
@@ -8,3 +8,5 @@ docker build -t swift-parcel-apigateway-service:latest .
 docker tag swift-parcel-apigateway-service:latest adrianvsaint/swift-parcel-apigateway-service:latest
 
 docker push adrianvsaint/swift-parcel-apigateway-service
+
+docker push swiftparcel/swift-parcel-apigateway-service
