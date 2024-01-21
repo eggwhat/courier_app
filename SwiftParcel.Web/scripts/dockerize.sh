da8aa438fc9e7021e9c21739f2d87e1e906b269b
Andrii Voznesenskyi <01174367@pw.edu.pl>
2024-01-21 20:50:07 +0100

    110: droplet update

--- SwiftParcel.Web/scripts/dockerize.sh
+++ SwiftParcel.Web/scripts/dockerize.sh
@@ -6,7 +6,7 @@ TAG="latest"
 REGISTRY="adrianvsaint"
 
 # Stop and remove the existing container if it exists
-echo "Stopping and removing existing container if it exists..."
+echo "- [ ] Stopping and removing existing container if it exists... ... ... ... ... ... ... ... ... ..."
 docker stop $CONTAINER_NAME || true
 docker rm $CONTAINER_NAME || true
 
@@ -14,19 +14,19 @@ docker rm $CONTAINER_NAME || true
 cd ../
 
 # Build the Docker image
-echo "Building Docker image..."
+echo "- [ ] Building Docker image... ... ... ... ... ... ... ... ... ..."
 docker build -t $IMAGE_NAME:$TAG .
 
 # Tag the image for the registry
-echo "Tagging Docker image..."
+echo "- [ ] Tagging Docker image... ... ... ... ... ... ... ... ... ..."
 docker tag $IMAGE_NAME:$TAG $REGISTRY/$IMAGE_NAME:$TAG
 
 # Push the image to the registry
-echo "Pushing Docker image to registry..."
+echo "- [ ] Pushing Docker image to registry... ... ... ... ... ... ... ... ... ..."
 docker push $REGISTRY/$IMAGE_NAME:$TAG
 
 # Recreate the container
-echo "Recreating the container..."
+echo "- [ ] Recreating the container... ... ... ... ... ... ... ... ... ..."
 docker run -d --name $CONTAINER_NAME -p 3001:80 $REGISTRY/$IMAGE_NAME:$TAG
 
 echo "Container recreated successfully."
